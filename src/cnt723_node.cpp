148fec4e615bdbe903c71a19062da7da3789c321
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2017-08-29 20:55:39 +0900

    Remove velocity calculation
    This should not be the job of the encoder because it involves
    unnecessary information such as the wheel diameter. Velocity calculation
    should be done in a higher-level driver, such as the node that publishes
    the /odom topic and tf transformation.

--- src/cnt723_node.cpp
+++ src/cnt723_node.cpp
@@ -2,9 +2,7 @@
 
 #include <ros/ros.h>
 #include <std_msgs/UInt32.h>
-#include <std_msgs/Float64.h>
 
-#include <cmath>
 #include <string>
 
 int
@@ -15,26 +13,13 @@ main(int argc, char* argv[]) {
     // Get parameters
     std::string port;
     int baudrate;
-    std::string topic_name_prefix;
-    std::string topic_name_count;
-    std::string topic_name_speed;
+    std::string topic_name;
     float frequency;
-    float wheel_diameter;
-    float counts_per_rotation;
 
     nh.getParam("port", port);
     nh.param("baudrate", baudrate, 9600);
-    nh.param("topic_name_prefix", topic_name_prefix, std::string{"encoder"});
-    nh.param("topic_name_count", topic_name_count, std::string{"count"});
-    nh.param("topic_name_speed", topic_name_speed, std::string{"speed"});
-    nh.param("frequency", frequency, static_cast<float>(1000));
-    nh.getParam("wheel_diameter", wheel_diameter);
-    nh.getParam("counts_per_rotation", counts_per_rotation);
-
-    if (topic_name_prefix != "") {
-        topic_name_count = topic_name_prefix + "/" + topic_name_count;
-        topic_name_speed = topic_name_prefix + "/" + topic_name_speed;
-    }
+    nh.param("topic_name", topic_name, std::string{"encoder_count"});
+    nh.param("frequency", frequency, static_cast<float>(100));
 
     Cnt723 cnt723{port, static_cast<unsigned>(baudrate)};
     try {
@@ -52,34 +37,14 @@ main(int argc, char* argv[]) {
         return 1;
     }
 
-    ros::Publisher count_pub = nh.advertise<std_msgs::UInt32>(topic_name_count, 1);
-    ros::Publisher speed_pub = nh.advertise<std_msgs::Float64>(topic_name_speed, 1);
+    ros::Publisher count_pub = nh.advertise<std_msgs::UInt32>(topic_name, 1);
     ros::Rate r{frequency};
 
     std_msgs::UInt32 msg_count;
-    std_msgs::Float64 msg_speed;
-    unsigned long long int last_count;
-    ros::Time last_count_time;
-    bool not_the_first = false;
     while (ros::ok()) {
-        auto current_count = cnt723.get_count();
-        auto current_time = ros::Time::now();
+        msg_count.data = cnt723.get_count();
 
-        if (not_the_first) {
-            auto dt = (current_time - last_count_time).toSec();
-            // Note: When in reverse, becomes negative
-            auto d_count = static_cast<long long int>(current_count - last_count);
-            auto speed = (d_count / counts_per_rotation) * (wheel_diameter * M_PI) / dt;
-
-            msg_count.data = current_count;
-            count_pub.publish(msg_count);
-            msg_speed.data = speed;
-            speed_pub.publish(msg_speed);
-        }
-
-        last_count = current_count;
-        last_count_time = current_time;
-        not_the_first = true;
+        count_pub.publish(msg_count);
 
         ros::spinOnce();
         r.sleep();
